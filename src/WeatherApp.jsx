47afc8d35c02c52a55ef6986031c4f0b6f71e452
CalvinaHarr <116849084+CalvinaHarr@users.noreply.github.com>
2025-01-07 21:25:48 +0000

    Designed basic UI with button styling.

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import './WeatherApp.css';
 
 const WeatherApp = () => {
     return (
-        <h1>Weather App</h1>
+        <div>
+            <h1>Weather App</h1>
+            <button className="cities">New York</button>
+            <button className="cities">Washington, D.C.</button>
+            <button className="cities">Los Angeles</button>
+        </div>
     );
 };
 
