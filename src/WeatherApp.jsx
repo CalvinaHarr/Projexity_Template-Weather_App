ad1cb911114e4ae4fbed777af1bfc13878e51487
CalvinaHarr <116849084+CalvinaHarr@users.noreply.github.com>
2025-01-08 01:06:29 +0000

    Made handleFetch function

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import './WeatherApp.css';
 
+const handleFetch = (lat, lon) => {
+    let BASE_URL = "https://api.weather.gov";
+    fetch(`https://api.weather.gov/${lat},${lon}`)
+        .then(response => response.json())
+        .catch(error => console.error('Error:', error));
+    console.log("big chungus");
+    alert("big chungus");
+};
+
 const WeatherApp = () => {
     return (
         <div>
             <h1>Weather App</h1>
-            <button className="cities">New York</button>
-            <button className="cities">Washington, D.C.</button>
-            <button className="cities">Los Angeles</button>
+            <button className="cities" onClick={() => handleFetch(38.8894, -77.0352)}>
+                New York</button>
+            <button className="cities" onClick={() => handleFetch(38.8894, -77.0352)}>
+                Washington, D.C.</button>
+            <button className="cities" onClick={() => handleFetch(38.8894, -77.0352)}>
+                Los Angeles</button>
         </div>
     );
 };
+
 
 export default WeatherApp;
