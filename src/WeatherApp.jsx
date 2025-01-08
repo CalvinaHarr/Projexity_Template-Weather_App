363113e62b88a520ed91adbcb34437737a6597a1
CalvinaHarr <116849084+CalvinaHarr@users.noreply.github.com>
2025-01-08 08:07:05 +0000

    Integrated API and logged weather data.

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import './WeatherApp.css';
 
-const handleFetch = (lat, lon) => {
-    let BASE_URL = "https://api.weather.gov";
-    fetch(`https://api.weather.gov/${lat},${lon}`)
-        .then(response => response.json())
-        .catch(error => console.error('Error:', error));
-    console.log("big chungus");
-    alert("big chungus");
+const handleFetch = async (lat, lon) => {
+    try{
+        const response = await fetch(`https://api.weather.gov/points/${lat},${lon}`);
+        const data = await response.json();
+        console.log(data);
+        return response;
+    } catch (error) {
+        console.error('Error:', error);
+    }
 };
 
 const WeatherApp = () => {
     return (
         <div>
             <h1>Weather App</h1>
-            <button className="cities" onClick={() => handleFetch(38.8894, -77.0352)}>
+            <button className="cities" onClick={() => handleFetch(40.7143, -74.0060)}>
                 New York</button>
             <button className="cities" onClick={() => handleFetch(38.8894, -77.0352)}>
                 Washington, D.C.</button>
-            <button className="cities" onClick={() => handleFetch(38.8894, -77.0352)}>
+            <button className="cities" onClick={() => handleFetch(34.0522, -118.2437)}>
                 Los Angeles</button>
         </div>
     );
